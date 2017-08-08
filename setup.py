f04ad9ecf80a4c6485ea58a769de96c5e5dc43be
Brian Carlson <briancarlson6174@gmail.com>
2017-08-08 15:14:25 -0400

    fixed urls

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ setup(
   description = 'A simple api for accessing the Confluence REST API',
   author = 'Brian Carlson',
   author_email = 'briancarlson6174@gmail.com',
-  url = 'https://github.com/TheRobotCarlson/PyConfluence', # use the URL to the github repo
-  download_url = 'https://github.com/TheRobotCarlson/PyConfluence/archive/0.1.tar.gz', # I'll explain this in a second
+  url = 'https://github.com/TheRobotCarlson/Confluency', # use the URL to the github repo
+  download_url = 'https://github.com/TheRobotCarlson/Confluency/archive/0.1.tar.gz', # I'll explain this in a second
   keywords = ['confluence', 'rest', 'api'], # arbitrary keywords
-  classifiers = [],
 )
