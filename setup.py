a51edb2747c43af807d960179cf39f4f687bd3a0
Brian Carlson <briancarlson6174@gmail.com>
2017-08-08 15:07:47 -0400

    fixed naming

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'Pyfluence',
-  packages = ['Pyfluence'], # this must be the same as the name above
+  name = 'Confluency',
+  packages = ['Confluency'], # this must be the same as the name above
   version = '0.1',
   description = 'A simple api for accessing the Confluence REST API',
   author = 'Brian Carlson',
   author_email = 'briancarlson6174@gmail.com',
-  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  url = 'https://github.com/TheRobotCarlson/PyConfluence', # use the URL to the github repo
+  download_url = 'https://github.com/TheRobotCarlson/PyConfluence/archive/0.1.tar.gz', # I'll explain this in a second
   keywords = ['confluence', 'rest', 'api'], # arbitrary keywords
   classifiers = [],
 )
