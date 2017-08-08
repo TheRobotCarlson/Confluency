cd465aea45fac3e514a58439e3b4ed6ec6d6de94
Brian Carlson <briancarlson6174@gmail.com>
2017-08-08 14:44:21 -0400

    moved files for pypi

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
   description = 'A simple api for accessing the Confluence REST API',
   author = 'Brian Carlson',
   author_email = 'briancarlson6174@gmail.com',
-  url = 'https://github.com/TheRobotCarlson/PyConfluence', # use the URL to the github repo
-  download_url = 'https://github.com/TheRobotCarlson/PyConfluence/archive/0.1.tar.gz', 
+  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
   keywords = ['confluence', 'rest', 'api'], # arbitrary keywords
   classifiers = [],
 )
