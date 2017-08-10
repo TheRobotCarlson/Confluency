8af80f3bf6fe660da82017d42c9385b74737a21b
TheRobotCarlson <briancarlson6174@gmail.com>
2017-08-10 14:46:47 -0400

    updated for PyPI

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 setup(
   name = 'Confluency',
   packages = ['Confluency'], # this must be the same as the name above
-  version = '0.4.3',
+  version = '0.4.4',
   description = 'A simple library for accessing the Confluence REST API',
   author = 'Brian Carlson',
   author_email = 'briancarlson6174@gmail.com',
   url = 'https://github.com/TheRobotCarlson/Confluency', # use the URL to the github repo
   download_url = 'https://github.com/TheRobotCarlson/Confluency/archive/0.1.tar.gz', # I'll explain this in a second
+  install_requires=['requests'],
   keywords = ['confluence', 'rest', 'api'], # arbitrary keywords
 )
